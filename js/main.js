3d6ebcb8be58c1210a7f9f4ebd3919d8b72a4281
sagehartly <sage.hartly@gmail.com>
2016-04-07 15:30:27 -0400

    Worked on js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // Add a class to the element //
 // Set a random top, left, width, height, rotation & opacity //
 
-var $banner = $('.banner');
+var $body = $('.banner');
 var $addStars;
 var $width = $banner.outerWidth();
 var $height = $banner.outerHeight();
@@ -11,4 +11,9 @@ for (i = 0; i < 75; i++) {
   $star = $('<div>');
   $star.addClass('star');
   $body.append($star);
-};
+  $star.css ({
+    top: Math.random() * height,
+    left: Math.random() * width,
+    opacity: Math.random(),
+    transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random() + ')'
+  });
